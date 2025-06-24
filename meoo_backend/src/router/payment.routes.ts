19d12ab4c6cf28c7830fbe03ee51ef66a0a89786
Kaaviyan <kaaviyan@pixalive.me>
2025-06-24 15:31:51 +0530

    Commit First One

--- meoo_backend/src/router/payment.routes.ts
+++ meoo_backend/src/router/payment.routes.ts
@@ -1,26 +1,26 @@
-import {Router} from 'express';
-import {paymentOrder,verifyPayment }from '../controller/payment.controller';
-import { checkQuery, checkRequestBodyParams } from '../middleware/Validators';
-import { basicAuthUser } from '../middleware/checkAuth';
-import { checkSession } from '../utils/tokenManager';
-const router:Router=Router();
+// import {Router} from 'express';
+// import {paymentOrder,verifyPayment }from '../controller/payment.controller';
+// import { checkQuery, checkRequestBodyParams } from '../middleware/Validators';
+// import { basicAuthUser } from '../middleware/checkAuth';
+// import { checkSession } from '../utils/tokenManager';
+// const router:Router=Router();
 
 
 
-router.post('/payment', // initiate payment
-    basicAuthUser,
-    // checkSession,
-    checkRequestBodyParams('_id'),
-    paymentOrder
-);
+// router.post('/payment', // initiate payment
+//     basicAuthUser,
+//     // checkSession,
+//     checkRequestBodyParams('_id'),
+//     paymentOrder
+// );
 
-router.get('/verify', // handle payment response
-    basicAuthUser,
-     //checkSession
-    checkQuery('orderId'),
-    verifyPayment,
+// router.get('/verify', // handle payment response
+//     basicAuthUser,
+//      //checkSession
+//     checkQuery('orderId'),
+//     verifyPayment,
    
-);
+// );
 
 
-export default router;
+// export default router;
